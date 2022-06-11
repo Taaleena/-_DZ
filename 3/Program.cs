38973182bf6d4400acb73638087bed2a5548ac8e
Talena <tillana@yandex.ru>
2022-06-11 19:23:57 +0300

    большее из трех

---
+++ 3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите последовательно три числа : ");
+
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            double max;
+
+max = a;
+
+if (b > a) max = b;
+
+if (c > max) max = c;
+{
+
+    Console.WriteLine("{0} большее число", max);
+
+
+}
