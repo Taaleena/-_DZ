051e4a8880b09ae321d755ae9b264f28d5d2be36
Talena <tillana@yandex.ru>
2022-06-11 19:37:52 +0300

    большее и меньшее из двух

---
+++ 4/Program.cs
@@ -0,0 +1,25 @@
+﻿        int a, b;
+ 
+        Console.WriteLine("Введите последовательно два числа : ");
+ 
+        a = Convert.ToInt32(Console.ReadLine());
+ 
+        b = Convert.ToInt32(Console.ReadLine());
+ 
+        if (a > b)
+ 
+        {
+ 
+            Console.WriteLine("{0} большее число", a);
+            Console.WriteLine("{0} меньшее число", b);
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine("{0} большее число ", b);
+            Console.WriteLine("{0} меньшее число ", a);
+ 
+        }
