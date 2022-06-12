cad4ecf5acc6e64939694755786998abc0461b9e
Talena <tillana@yandex.ru>
2022-06-12 18:34:33 +0300

    удаление второй цифры из рандомного трехзначного числа

---
+++ 8/Program.cs
@@ -0,0 +1,14 @@
+﻿int Number = new Random().Next(100, 1000);
+int NewNumber = 0;
+
+int NumberWithoutSecDig()
+{
+    NewNumber = Number / 100 * 10 + Number % 10;
+    return NewNumber;
+}
+
+NumberWithoutSecDig();
+
+
+System.Console.WriteLine($"Рандомное число {Number}");
+System.Console.WriteLine($"Если удалить вторую цифру, получится число {NewNumber}");
