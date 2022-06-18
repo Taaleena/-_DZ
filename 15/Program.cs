817a5467a8ca2370bed06664fa0f94254d2d109f
Talena <tillana@yandex.ru>
2022-06-18 21:18:28 +0300

    массив из восьми чисел

---
+++ 15/Program.cs
@@ -0,0 +1,19 @@
+﻿int N = 8;
+
+int[] SetArray(int x)
+{
+    int[] arr = new int[x];
+    for (int i = 0; i < x; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        System.Console.Write($"{array[i]} ");
+}
+
+PrintArray(SetArray(N));
