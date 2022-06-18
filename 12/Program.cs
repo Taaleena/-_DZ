b60400a3834e3581d37fda91bea5cda94a337883
Talena <tillana@yandex.ru>
2022-06-18 20:41:40 +0300

    кубы

---
+++ 12/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Введите число:   ");
+string param = Console.ReadLine();
+int N = Convert.ToInt32(param);
+int count = 0;
+
+while (count < N)
+
+{
+count++;
+System.Console.WriteLine(count * count * count);
+}
