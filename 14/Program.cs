5bd919b618385489888d74b4589421b22906dd79
Talena <tillana@yandex.ru>
2022-06-18 21:14:57 +0300

    сумма числа

---
+++ 14/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int number = new Random().Next(1, 100);
+int sum = 0;
+System.Console.Write($"Сумма {number} равна ");
+
+while (number > 0)
+{
+    int digit = number % 10;
+    sum = sum + digit;
+    number = number / 10;
+}
+
+System.Console.WriteLine(sum);
+System.Console.WriteLine();
