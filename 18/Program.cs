8da1bd37480e4ab3f380f84e05a7ac8613d7be17
Talena <tillana@yandex.ru>
2022-06-25 13:54:59 +0300

    мин макс вещественного массива

---
+++ 18/Program.cs
@@ -0,0 +1,27 @@
+﻿int size = new Random().Next(10, 13);
+
+System.Console.WriteLine($"Элементов массива: {size}");
+
+double[] FillSourceArray(int size)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < size; i++)
+    { 
+               array[i] = new Random().NextDouble() * 100;
+    }
+    return array;
+}
+
+void PrintMinMax(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.WriteLine(arr[i]);
+    }
+    double difference = arr.Max() - arr.Min();
+    System.Console.WriteLine($"Минимальное значение {arr.Min()}, максимальное значение {arr.Max()}");
+    System.Console.WriteLine($"Разница между минимальным и максимальным значениями составляет {difference}");
+    System.Console.WriteLine();
+}
+
+PrintMinMax(FillSourceArray(size));
