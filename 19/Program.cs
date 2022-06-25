bb5bee4c65e11e880e1545752e59d92460a5b58d
Talena <tillana@yandex.ru>
2022-06-25 14:04:44 +0300

    введено положительных

---
+++ 19/Program.cs
@@ -0,0 +1,13 @@
+﻿int volume = 5;                // колчествопредложений ввода
+int count = 0;
+
+while (volume > 0)
+{
+    System.Console.Write("Введите число:   ");
+    string num = Console.ReadLine();
+    int number = Convert.ToInt32(num);
+    if (number > 0) count++;
+    volume--;
+}
+
+System.Console.WriteLine($"Чисел больше 0: {count}");
