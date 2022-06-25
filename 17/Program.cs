d2fc6b41b245d0db4cfdc605ed3f41a30a098394
Talena <tillana@yandex.ru>
2022-06-25 13:44:13 +0300

    сумма чисел на нечетной позиции

---
+++ 17/Program.cs
@@ -0,0 +1,27 @@
+﻿int size = 13;
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 50);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    int sum = 0;
+    
+    for (int i = 0; i < size; i++)
+    {
+        System.Console.Write(arr[i] + " ");
+        if (i % 2 > 0) sum += arr[i];
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Сумма чисел на нечетной позиции - {sum} ");
+   
+}
+
+PrintArray(FillArray(size));
