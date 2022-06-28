2e6922f07c6c61de5f4755575cae266a43979d91
Talena <tillana@yandex.ru>
2022-06-28 22:07:54 +0300

    точка пересечения

---
+++ 20/Program.cs
@@ -0,0 +1,36 @@
+﻿
+System.Console.Write ("Введите число b1:   ");
+string number = Console.ReadLine();
+double b1 = Convert.ToInt32(number);
+
+System.Console.Write ("Введите число k1:   ");
+string number2 = Console.ReadLine();
+double k1 = Convert.ToInt32(number2);
+
+System.Console.Write ("Введите число b2:   ");
+string number3 = Console.ReadLine();
+double b2 = Convert.ToInt32(number3);
+
+System.Console.Write ("Введите число k2:   ");
+string number4 = Console.ReadLine();
+double k2 = Convert.ToInt32(number4);
+
+double x (double i)
+{
+double result = i;
+i=(b2 - b1)/(k1 - k2);
+return result;
+}
+
+double touch (double m);
+
+{
+double result = m;
+m = k1*x+b1;
+return result;
+
+}
+
+System.Console.WriteLine($"точка пересечения {touch}");
+
+
