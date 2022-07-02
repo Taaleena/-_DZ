d3f41f563380c714e729242e9738587cd3433e3e
Talena <tillana@yandex.ru>
2022-07-02 20:20:39 +0300

    двумерный массив с вещественными числами

---
+++ 22/Program.cs
@@ -0,0 +1,31 @@
+﻿int m = 6;
+int n = 3;
+
+double[,] Array2(int m, int n)
+{
+    double[,] table = new double[m, n];
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = new Random().NextDouble() * 100;
+        }
+    }
+    return table;
+}
+
+void PrintTable(double[,] tab)
+{
+    for (int i = 0; i < tab.GetLength(0); i++)
+    {
+        for (int j = 0; j < tab.GetLength(1); j++)
+        {
+            System.Console.Write(tab[i, j] + "    ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+double[,] TableDouble = Array2(m, n);
+PrintTable(TableDouble);
