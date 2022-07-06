814e1560ad78e042971557ea22a5b21b22ac62c2
Talena <tillana@yandex.ru>
2022-07-06 20:40:09 +0300

    строки по убыванию

---
+++ 25/Program.cs
@@ -0,0 +1,56 @@
+﻿int row = 4;
+int col = 7;
+int min = 10;
+int max = 100;
+
+int[,] GetMassiv(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+int[,] table = GetMassiv(row, col, min, max);
+PrintTable(table);
+
+int[,] SortRowAscending(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int minPosition = j;                                            
+            for (int k = j + 1; k < array.GetLength(1); k++)                
+            {
+                if (array[i, k] > array[i, minPosition]) minPosition = k;    
+            }
+            int tmp = array[i, j];                                          
+            array[i, j] = array[i, minPosition];                            
+            array[i, minPosition] = tmp;                                    
+        }
+
+    }
+    return array;
+}
+
+void PrintTable(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.WriteLine();
+int[,] rowSort = SortRowAscending(table);
+PrintTable(rowSort);
