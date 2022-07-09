135e9ac08febaa0ff106b8792c9f7c8a26ef24a7
Talena <tillana@yandex.ru>
2022-07-09 22:16:16 +0300

    рекурсия сумма

---
+++ 30/Program.cs
@@ -0,0 +1,44 @@
+﻿int m = 1;
+int n = 6;
+
+
+int Sum (int m, int n)
+{
+
+if (m > n) return 0;
+
+
+return Sum (m, n-1)+n;
+
+}
+
+int i = Sum (m, n);
+
+System.Console.WriteLine();
+Console.WriteLine($"сумма  {i}");
+
+
+
+
+
+
+// int Sum (int m, int n, int i)
+// {
+
+// if (m > n) 
+// {
+//    Console.WriteLine (m + " ");
+//    i = i + m;
+//    m++;
+   
+   
+//  return
+//   Sum (m, n, i); 
+//    }
+// return i;
+// }
+
+//  Sum (m, n, i);
+
+// System.Console.WriteLine();
+// Console.WriteLine($"сумма  {Sum}");
