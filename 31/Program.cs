653938ad77ef2558d9930ed0f280e581d02aef0e
Talena <tillana@yandex.ru>
2022-07-09 22:58:44 +0300

    Akkerman

---
+++ 31/Program.cs
@@ -0,0 +1,12 @@
+﻿int m = 3;
+int n = 7;
+
+int Ackermann (int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m != 0 && n == 0) return Ackermann(m - 1, 1);
+    if (m > 0 && n > 0) return Ackermann(m - 1, Ackermann(n - 1));
+return Ackermann(m, n);
+}
+
+Console.WriteLine($"Функция ({m},{n}) = {Ackermann(m, n)}");
