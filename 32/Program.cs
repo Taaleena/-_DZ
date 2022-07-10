4e5af1fce2ef117334ea3439670db0c5562d55ff
Talena <tillana@yandex.ru>
2022-07-10 14:38:44 +0300

    последовательность

---
+++ 32/Program.cs
@@ -0,0 +1,24 @@
+﻿int n = 8;
+int num = 1;
+
+
+
+static void sequence (int n, int num)
+{
+    
+    for (int i = 1; i <= n; i++)
+    
+    if (i == (1+num) * num / 2)
+   
+    {
+        num++;
+Console.Write($"{num}, ");
+    }
+        
+}
+    sequence (n, num);
+    System.Console.Write($"{num}, ");      
+        
+        
+        
+        
